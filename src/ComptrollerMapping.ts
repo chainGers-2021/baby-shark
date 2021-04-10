63c88998e2ea0f9e0c99bf599f1196bd818f2cea
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2021-04-10 16:11:05 +0530

    ✨ adds mappings for newTokenAdded

--- src/ComptrollerMapping.ts
+++ src/ComptrollerMapping.ts
@@ -4,6 +4,13 @@ import {
   newTokenAdded,
 } from "../generated/Comptroller/Comptroller";
 
+import { Token } from "../generated/schema";
+
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handlenewTokenAdded(event: newTokenAdded): void {}
+export function handlenewTokenAdded(event: newTokenAdded): void {
+  let token = new Token(event.params._symbol);
+  token.address = event.params._token.toHexString();
+
+  token.save();
+}
